501acf32fb02096b9660caa938c0d9f3021a6cb0
StoneT2000 <stoneztao@gmail.com>
2018-09-16 14:52:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Polytomizator (Electron Build)
---
+++ polytomizator-darwin-x64/Polytomizator.app/Contents/Resources/app/app.js
@@ -0,0 +1,31 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let window = null
+
+// Wait until the app is ready
+app.once('ready', () => {
+  // Create a new window
+  window = new BrowserWindow({
+    // Set the initial width to 500px
+    width: 1000,
+    // Set the initial height to 400px
+    height: 800,
+    // set the title bar style
+    titleBarStyle: 'hidden-inset',
+    // set the background color to black
+    // Don't show the window until it's ready, this prevents any white flickering
+    show: false
+  })
+
+  window.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  window.once('ready-to-show', () => {
+    window.show()
+  })
+})
